8ac9e5955c4841086c8371c56f2755d52a9df536
Chien Vu Hoang <chien.vh@gmail.com>
2015-09-18 23:13:44 +0700

    Day 7: Calling Stored Procedures in Asp.Net C#

---
+++ LearningProgrammingByExample/Repeater_UsingStoreProcedure.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Repeater_UsingStoreProcedure.aspx.cs" Inherits="LearningProgrammingByExample.Repeater_UsingStoreProcedure" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+
+        .auto-style1 {
+            width: 61%;
+            height: 80px;
+        }
+
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Name:
+        <asp:TextBox ID="txtName" runat="server" Width="277px"></asp:TextBox>
+        <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+        <br />
+        <br />
+            <asp:Repeater ID="rptMyRepeater" runat="server">
+                <HeaderTemplate>
+                    <table class="auto-style1" border="1">
+                        <tr>
+                            <td colspan="4"><strong>List Person</strong></td>
+                        </tr>
+                        <tr>
+                            <td>Id</td>
+                            <td>Name</td>
+                            <td>Address</td>
+                            <td>Created Date</td>
+                        </tr>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <tr>
+                        <td><%#Eval("Id") %></td>
+                        <td><%#Eval("Name") %></td>
+                        <td><%#Eval("Address") %></td>
+                        <td><%#Eval("CreatedDate") %></td>
+                    </tr>
+                </ItemTemplate>
+                <FooterTemplate>
+                    </table>
+                </FooterTemplate>
+            </asp:Repeater>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ LearningProgrammingByExample/Repeater_UsingStoreProcedure.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LearningProgrammingByExample
+{
+    public partial class Repeater_UsingStoreProcedure : System.Web.UI.Page
+    {
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        string connectionString = WebConfigurationManager.ConnectionStrings["stringConnection"].ConnectionString;
+        SqlConnection con;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            LoadingDataToReapter();
+        }
+
+        private void LoadingDataToReapter()
+        {
+            con = new SqlConnection(connectionString);
+            try
+            {
+                cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "spGetPersonList";
+                cmd.Connection = con;
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                //Bind data into reapter
+                rptMyRepeater.DataSource = reader;
+                rptMyRepeater.DataBind();
+                //Close connections
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectionString);
+            try
+            {
+                cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "spGetPersonListByName";
+                cmd.Parameters.AddWithValue("@name",txtName.Text.Trim());
+                cmd.Connection = con;
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                //Bind data into reapter
+                rptMyRepeater.DataSource = reader;
+                rptMyRepeater.DataBind();
+                //Close connections
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
