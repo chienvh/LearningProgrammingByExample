71cde8d4a86cc0f036b3307cee2842b766d76053
Chien Vu Hoang <chien.vh@gmail.com>
2015-10-01 01:22:10 +0700

    Day 9 - Nested Repeater Controls in ASP.Net C#

---
+++ LearningProgrammingByExample/NestedRepeater_Example.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NestedRepeater_Example.aspx.cs" Inherits="LearningProgrammingByExample.NestedRepeater_Example" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 61%;
+            height: 80px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Repeater ID="rptProductTypeParent" runat="server" OnItemDataBound="rptProductTypeParent_ItemDataBound">
+                <ItemTemplate>
+                    <p>
+                        <b>Id: <%#Eval("productTypeId") %></b> &nbsp;<asp:HiddenField ID="hdfProductTypeId" runat="server" Value='<%#Eval("productTypeId") %>' />
+                        <b>Type Name: <%#Eval("productTypeName") %></b>
+                    </p>
+
+                    <p>
+                        <asp:Repeater ID="rptProductChild" runat="server">
+                            <ItemTemplate>
+                                <p> 
+                                    - Product Name: <%#Eval("productName") %>&nbsp;
+                                    Price: <%#Eval("productPrice") %>&nbsp;
+                                    Quantity: <%#Eval("quantity") %>
+                                </p>
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </p>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LearningProgrammingByExample/NestedRepeater_Example.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LearningProgrammingByExample
+{
+    public partial class NestedRepeater_Example : System.Web.UI.Page
+    {
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        string connectionString = WebConfigurationManager.ConnectionStrings["stringConnection"].ConnectionString;
+        SqlConnection con;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            LoadingDataToReapter();
+        }
+
+        private void LoadingDataToReapter()
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+            try
+            {
+                string sql = "Select productTypeId, productTypeName from tblProductType Order by productTypeId desc";
+                cmd = new SqlCommand(sql, con);
+                da = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+                da.Fill(ds);
+                //Bind data into reapter
+                rptProductTypeParent.DataSource = ds;
+                rptProductTypeParent.DataBind();
+                //Close connections
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void rptProductTypeParent_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.AlternatingItem || e.Item.ItemType == ListItemType.Item)
+            {
+                var hdfProductType = (HiddenField)e.Item.FindControl("hdfProductTypeId");
+                var rptProductChild = (Repeater)e.Item.FindControl("rptProductChild");
+
+                string sql = "Select [productId],[productTypeId],[productName],[productPrice],[quantity] from [dbo].[tblProduct] where productTypeId = @productTypeId Order By productId desc";
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@productTypeId", Convert.ToInt32(hdfProductType.Value));
+                da = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+                da.Fill(ds);
+                //Bind data into reapter
+                rptProductChild.DataSource = ds;
+                rptProductChild.DataBind();
+                //Close connections
+                cmd.Dispose();
+                con.Close();
+            }
+        }
+    }
+}
